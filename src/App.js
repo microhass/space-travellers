6b67cac0bbdec2d2edeb222366a940d38d5cc771
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-24 20:54:11 +0200

    Display Mission

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
-import Error from './components/Error';
+import Missions from './components/Missions';
+// import Error from './components/Error';
 
 import './App.css';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<Home />} />
-        <Route path="*" element={<Error />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <>
+    <Header />
+    <Missions />
+    <Home />
+  </>
 );
 
 export default App;
