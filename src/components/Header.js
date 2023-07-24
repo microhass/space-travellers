21580cfb3fc39ab81350fcc7e367ad7b8c8de16d
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-24 20:54:48 +0200

    Modify header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,6 @@
-import React from 'react';
-import { Link, NavLink, Outlet } from 'react-router-dom';
-import StyledHeader from '../styles/styledHeader';
-import planetImage from '../images/planet.png';
-
-const links = [
-  {
-    name: 'rockets',
-    href: '/rockets',
-  },
-  {
-    name: 'missions',
-    href: '/missions',
-  },
-  {
-    name: 'my profile',
-    href: '/profile',
-  },
-];
-
 const Header = () => (
-  <>
-    <StyledHeader>
-      <nav>
-        <div>
-          <Link to="/">
-            <img src={planetImage} alt="Logo" />
-            <h1>Space Travellers hub</h1>
-          </Link>
-        </div>
-        <ul className="links">
-          {links.map((link) => (
-            <li key={link.name}>
-              <NavLink
-                to={link.href}
-                className={({ isActive }) => (isActive ? 'active' : '')}
-              >
-                {link.name}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </StyledHeader>
-    <Outlet />
-  </>
+  <nav>
+    <h1>Header</h1>
+  </nav>
 );
 export default Header;
