192bb6109f693d0ce3a655a2d6dd1f9a6f226b32
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-25 07:43:14 +0200

    Add fetched data to store

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMissionsData } from '../feature/missions/missionsSlice';
 
 const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissionsData());
+  }, [dispatch]);
   const { missionData } = useSelector((store) => store.missions);
   return (
     <div>
@@ -11,14 +16,16 @@ const Missions = () => {
             <th>Mission</th>
             <th>Desciption</th>
             <th>Status</th>
+            <th> </th>
           </tr>
         </thead>
         <tbody>
           {missionData.map((mission) => (
             <tr key={mission.id}>
-              <td>{mission.id}</td>
-              <td>{mission.title}</td>
+              <td>{mission.name}</td>
               <td>{mission.description}</td>
+              <td> Member/Not a member</td>
+              <td> Join/Leave</td>
             </tr>
           ))}
         </tbody>
